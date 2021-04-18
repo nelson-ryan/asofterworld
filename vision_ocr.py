7f3c4231fbba9b5b610f4303087aefb50a9c825c
Ryan Nelson <nelsonr@email.arizona.edu>
2021-04-18 14:30:30 -0700

    Move detect_text code to function. Change printed text to only the first element.
    The output of the Google OCR first has the combined text, then each individual word. Removed the loop becuase each individual word isn't necessary, keeping only the first element.

--- vision_ocr.py
+++ vision_ocr.py
@@ -8,50 +8,49 @@ Testing the Google Cloud Vision API for OCR
 """
 
 import os
+from google.cloud import vision
+import io
+
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "nomadic-zoo-293819-8ccfdaa58681.json"
 
 path = "comics/0702_graverobber.jpg"
 
+def detect_text(path):
+    """Detects text in the file."""
 
-#def detect_text(path):
-"""Detects text in the file."""
-from google.cloud import vision
-# from google.cloud.vision import types # No longer necessary from version 2.0.0????
-import io
-client = vision.ImageAnnotatorClient()
+    client = vision.ImageAnnotatorClient()
 
-with io.open(path, 'rb') as image_file:
-    content = image_file.read()
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
 
-image = vision.Image(content=content)
+    image = vision.Image(content=content)
 
-response = client.text_detection(image=image)
-texts = response.text_annotations
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
 
-#print(type(texts))
+    #print(type(texts))
 
-print('Texts:')
+    print('Texts:')
 
-for text in texts:
-    print('\n"{}"'.format(text.description))
+    print('\n"{}"'.format(texts[0].description))
 
-#     vertices = (['({},{})'.format(vertex.x, vertex.y)
-#                 for vertex in text.bounding_poly.vertices])
+    #     vertices = (['({},{})'.format(vertex.x, vertex.y)
+    #                 for vertex in text.bounding_poly.vertices])
 
-#     print('bounds: {}'.format(','.join(vertices)))
+    #     print('bounds: {}'.format(','.join(vertices)))
 
-'''
-Within 'texts' are objects for 'description', which is the read text,
-and appears to be separated by word, and also includes new lines.
-These are an EntityAnnotation type, but can be converted to str()
+    '''
+    Within 'texts' are objects for 'description', which is the read text,
+    and appears to be separated by word, and also includes new lines.
+    These are an EntityAnnotation type, but can be converted to str()
+    
+    So I think I can just convert to string and return the result to the main function
+    '''
 
-So I think I can just convert to string and return the result to the main function
-'''
-
-if response.error.message:
-    raise Exception(
-        '{}\nFor more info on error messages, check: '
-        'https://cloud.google.com/apis/design/errors'.format(
-            response.error.message))
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
         
-#detect_text("comics/0001_myparents.jpg")
+detect_text(path)
