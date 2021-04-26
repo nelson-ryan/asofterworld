85a0cbe4a9f0daf1e2ea1e8a3e34a63514fb56dd
Ryan Nelson <nelsonr@email.arizona.edu>
2021-04-25 23:53:16 -0700

    Refine contour detection, Add func split_frames
    - Correct frame order to prioritize x-axis placement
    
    - Remove median blur step prior to find contours, thereby removing
      footer text from some frame boundaries

--- comicsplit2.py
+++ comicsplit2.py
@@ -6,56 +6,55 @@ Created on Wed Oct 28 09:53:37 2020
 """
 
 # bulk of code from https://stackoverflow.com/a/56473372
+# solution to ordering from https://stackoverflow.com/a/39445901
 
-# solution to ordering modified from https://stackoverflow.com/a/39445901
-
-
-import cv2 # OpenCV (opencv-python)
+import cv2  # OpenCV (opencv-python)
 import os
 
-if not os.path.exists("split-test/"):
-    os.mkdir("split-test/")
 
-# Load image
-im = cv2.imread('comics/0392_sit.jpg', cv2.IMREAD_UNCHANGED)
+def split_frames(filename, dest_folder="split-frames/"):
+    if not os.path.exists(dest_folder):
+        os.mkdir(dest_folder)
 
-# Create greyscale version
-gr = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    title = filename.split(sep="/")[-1].split(sep=".")[-2]
+    print(title)
+    # Load image
+    im = cv2.imread(filename, cv2.IMREAD_UNCHANGED)
 
-# Threshold to get black and white
-_,grthresh = cv2.threshold(gr, 230, 255, cv2.THRESH_BINARY)
-cv2.imwrite('split-test/result-1.png', grthresh)
+    # Create greyscale version
+    gr = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    # Threshold to get black and white
+    _, grthresh = cv2.threshold(gr, 230, 255, cv2.THRESH_BINARY)
 
-# Median filter to remove JPEG noise
-grthresh = cv2.medianBlur(grthresh, 11)
-cv2.imwrite('split-test/result-2.png', grthresh)
+    # Find contours
+    contours, hierarchy = cv2.findContours(grthresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-# Find contours
-contours, hierarchy = cv2.findContours(grthresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    # This function comes from https://stackoverflow.com/a/39445901
+    def get_contour_precedence(contour, cols):
+        tolerance_factor = 50
+        origin = cv2.boundingRect(contour)
+        return (origin[1] // tolerance_factor) * cols + origin[0]
+
+    contours.sort(key=lambda y: get_contour_precedence(y, im.shape[0]))
+
+    # From https://stackoverflow.com/a/56473372 again
+    # Look through contours, checking what we found
+    frame = 0
+    for i in range(len(contours)):
+        area = cv2.contourArea(contours[i])
+        # Only consider ones taller than around 100 pixels and wider than about 300 pixels
+        if area > 30000:
+            # Get cropping box and crop
+            rc = cv2.minAreaRect(contours[i])
+            box = cv2.boxPoints(rc)
+            Xs = [box[0, 0], box[1, 0], box[2, 0], box[3, 0]]
+            Ys = [box[0, 1], box[1, 1], box[2, 1], box[3, 1]]
+            x0 = int(round(min(Xs)))
+            x1 = int(round(max(Xs)))
+            y0 = int(round(min(Ys)))
+            y1 = int(round(max(Ys)))
+            cv2.imwrite(f'split-test/{title}-{frame:02d}.png', im[y0:y1, x0:x1])
+            frame += 1
 
 
-def get_contour_precedence(contour, cols):
-    tolerance_factor = 1
-    origin = cv2.boundingRect(contour)
-    return (origin[0] // tolerance_factor) * cols + origin[0]
-
-
-contours.sort(key=lambda y: get_contour_precedence(y, im.shape[0]))
-
-# Look through contours, checking what we found
-frame = 0
-for i in range(len(contours)):
-    area = cv2.contourArea(contours[i])
-    # Only consider ones taller than around 100 pixels and wider than about 300 pixels
-    if area > 30000:
-        # Get cropping box and crop
-        rc = cv2.minAreaRect(contours[i])
-        box = cv2.boxPoints(rc)
-        Xs = [box[0, 0], box[1, 0], box[2, 0], box[3, 0]]
-        Ys = [box[0, 1], box[1, 1], box[2, 1], box[3, 1]]
-        x0 = int(round(min(Xs)))
-        x1 = int(round(max(Xs)))
-        y0 = int(round(min(Ys)))
-        y1 = int(round(max(Ys)))
-        cv2.imwrite(f'split-test/frame-{frame}.png', im[y0:y1, x0:x1])
-        frame += 1
+split_frames('comics/0753_purina.jpg')
