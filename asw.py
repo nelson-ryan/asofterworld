aee19df0d48e1ff1d3cfb91928b3fa9f0081d1db
Ryan Nelson <nelsonr@email.arizona.edu>
2021-05-02 01:49:33 -0700

    Incorporate comicsplit into main funtion.
    Main funtion will now split and save frames from all comics that it pulled from the website in the same run (although this defeats the purpose of pre-saving because saving them all is time-consuming

--- asw.py
+++ asw.py
@@ -13,26 +13,30 @@ import bs4  # beautifulsoup4
 import requests
 import os  # for directory checking and creation
 
+import comicsplit2
+#import vision_ocr #
 
-# UPDATE FIRST COMIC NUMBER IN FUNCTION CALL
+# UPDATE FIRST COMIC NUMBER IN VARIABLE DECLARATION
 def main():
     comics = []
-    currentcomic = 1245
+    current_comic = 1245
     while True:
-        gotcomic = save_comic(currentcomic)
-        if gotcomic:
-            comics.append(gotcomic)
-            currentcomic += 1
+        got_comic = save_comic(current_comic)
+        if got_comic:
+            comics.append(got_comic)
+            current_comic += 1
         else:
             break
+
+    # Use same destination path stored in dict by save_comic
     for i in range(len(comics)):
-        print(comics[i])
+        comicsplit2.split_frames(comics[i].get("save_loc"))
 
 
 def save_comic(n):
-    comicdict = {}
-    comicnumber = n
-    res = requests.get(f'https://www.asofterworld.com/index.php?id={comicnumber}')
+    comic_dict = {}
+    comic_number = n
+    res = requests.get(f'https://www.asofterworld.com/index.php?id={comic_number}')
     res.raise_for_status()
     
     soup = bs4.BeautifulSoup(res.text, "html.parser")
@@ -44,21 +48,22 @@ def save_comic(n):
     # If there's no filename, there's no comic, so stop
     if not filename:
         return
+    alt_text = comic[0].get('title')
 
-    alttext = comic[0].get('title')
-
-    comicdict["comicnumber"] = comicnumber
-    comicdict["filename"] = filename
-    comicdict["alttext"] = alttext
+    img_url = requests.get(url)
 
     if not os.path.exists('comics/'):
         os.mkdir('comics/')
+    save_loc = f'comics/{comic_number:04d}_{filename}'
+    with open(save_loc, 'wb') as img:
+        img.write(img_url.content)
 
-    imgurl = requests.get(url)
-    with open(f'comics/{comicnumber:04d}_{filename}', 'wb') as img:
-        img.write(imgurl.content)
+    comic_dict["comic_number"] = comic_number
+    comic_dict["filename"] = filename
+    comic_dict["alt_text"] = alt_text
+    comic_dict["save_loc"] = save_loc
 
-    return comicdict
+    return comic_dict
+
 
 main()
-
